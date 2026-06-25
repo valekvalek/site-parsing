1808386af0f720de64febdc1082c535801f74e3b
Valentin <claryty86@gmail.com>
2026-06-25 18:25:21 +0300

    fix: add slug field to sources config

--- src/config.js
+++ src/config.js
@@ -3,12 +3,14 @@ export const sources = [
     api: 'https://legendamarusino.ru/api/realty-filter/custom/real-estates',
     site: 'https://legendamarusino.ru',
     project: 'ЖК "Легенда Марусино"',
+    slug: 'legendamarusino',
     projectId: 'a5f9b6b9-037d-4cd8-981c-cbd55e93a5c0'
   },
   {
     api: 'https://legendakorenevo.ru/api/realty-filter/custom/real-estates',
     site: 'https://legendakorenevo.ru',
     project: 'ЖК "Легенда Коренёво"',
+    slug: 'legendakorenevo',
     projectId: '61b193a5-aa22-4f3a-bf22-216ebc5648b1'
   }
 ];
